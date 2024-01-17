677cda4118be0b196b711d12de4724f049da54a7
github_JevonnCobin <jmcobin@me.com>
2024-01-17 04:13:02 -0600

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+"# Million-Dollars-But" 
---
+++ mdb.js
@@ -0,0 +1,200 @@
+function test(){
+    var million1 = document.getElementsByName("million1");
+    var check1 = 0;
+    for(i=0;i<million1.length;i++){
+      if(million1[i].checked){
+        check1++;
+        break;
+      }
+    }
+    var million2 = document.getElementsByName("million2");
+    var check2 = 0;
+    for(i=0;i<million2.length;i++){
+      if(million2[i].checked){
+        check2++;
+        break;
+      }
+    }
+     var million3 = document.getElementsByName("million3");
+    var check3 = 0;
+    for(i=0;i<company.length;i++){
+      if(company[i].checked){
+        check2++;
+        break;
+      }
+    }
+     var million4 = document.getElementsByName("million4");
+    var check4 = 0;
+    for(i=0;i<million4.length;i++){
+      if(million4[i].checked){
+        check4++;
+        break;
+      }
+    }
+     var million5 = document.getElementsByName("million5");
+    var check5 = 0;
+    for(i=0;i<million5.length;i++){
+      if(million5[i].checked){
+        check5++;
+        break;
+      }
+    }
+     var million6 = document.getElementsByName("million6");
+    var check6 = 0;
+    for(i=0;i<million6.length;i++){
+      if(million6[i].checked){
+        check6++;
+        break;
+      }
+    }
+     var million7 = document.getElementsByName("million7");
+    var check7 = 0;
+    for(i=0;i<million7.length;i++){
+      if(million7[i].checked){
+        check7++;
+        break;
+      }
+    }
+     var million8 = document.getElementsByName("million8");
+    var check8 = 0;
+    for(i=0;i<million8.length;i++){
+      if(million8[i].checked){
+        check8++;
+        break;
+      }
+    }
+     var million9 = document.getElementsByName("million9");
+    var check9 = 0;
+    for(i=0;i<million9.length;i++){
+      if(million9[i].checked){
+        check9++;
+        break;
+      }
+    }
+    var million10 = document.getElementsByName("million10");
+    var check10 = 0;
+    for(i=0;i<million10.length;i++){
+      if(million10[i].checked){
+        check10++;
+        break;
+      }
+    }
+    var million11 = document.getElementsByName("million11");
+    var check11 = 0;
+    for(i=0;i<million11.length;i++){
+      if(million11[i].checked){
+        check11++;
+        break;
+      }
+    }
+    var million12 = document.getElementsByName("million12");
+    var check12 = 0;
+    for(i=0;i<million12.length;i++){
+      if(million12[i].checked){
+        check12++;
+        break;
+      }
+    }
+    var million13 = document.getElementsByName("million13");
+    var check13 = 0;
+    for(i=0;i<million13.length;i++){
+      if(million13[i].checked){
+        check13++;
+        break;
+      }
+    }
+    var million14 = document.getElementsByName("million14");
+    var check14 = 0;
+    for(i=0;i<million14.length;i++){
+      if(million14[i].checked){
+        check14++;
+        break;
+      }
+    }
+    var million15 = document.getElementsByName("million15");
+    var check15 = 0;
+    for(i=0;i<million15.length;i++){
+      if(million15[i].checked){
+        check15++;
+        break;
+      }
+    }
+    var million16 = document.getElementsByName("million16");
+    var check16 = 0;
+    for(i=0;i<million16.length;i++){
+      if(million16[i].checked){
+        check16++;
+        break;
+      }
+    }
+    var million17 = document.getElementsByName("million17");
+    var check17 = 0;
+    for(i=0;i<million17.length;i++){
+      if(million17[i].checked){
+        check17++;
+        break;
+      }
+    }
+    var million18 = document.getElementsByName("million18");
+    var check18 = 0;
+    for(i=0;i<million18.length;i++){
+      if(million18[i].checked){
+        check18++;
+        break;
+      }
+    }var million19 = document.getElementsByName("million19");
+    var check19 = 0;
+    for(i=0;i<million19.length;i++){
+      if(million19[i].checked){
+        check19++;
+        break;
+      }
+    }
+    var million20 = document.getElementsByName("million20");
+    var check20 = 0;
+    for(i=0;i<million20.length;i++){
+      if(million20[i].checked){
+        check20++;
+        break;
+      }
+    }
+    var million21 = document.getElementsByName("million21");
+    var check21 = 0;
+    for(i=0;i<million21.length;i++){
+      if(million21[i].checked){
+        check21++;
+        break;
+      }
+    }var million22 = document.getElementsByName("million22");
+    var check22 = 0;
+    for(i=0;i<million22.length;i++){
+      if(million22[i].checked){
+        check22++;
+        break;
+      }
+    }
+    var million23 = document.getElementsByName("million23");
+    var check23 = 0;
+    for(i=0;i<million23.length;i++){
+      if(million23[i].checked){
+        check23++;
+        break;
+      }
+    }
+    var million24 = document.getElementsByName("million24");
+    var check24 = 0;
+    for(i=0;i<million24.length;i++){
+      if(million24[i].checked){
+        check24++;
+        break;
+      }
+    }
+    if(check1 && check2 && check3 && check4 && check5 && check6 && check7&& check8
+    && check9 && check10 && check11 && check12 && check13 && check14 && check15 && check16
+    && check17 && check18 && check19 && check20 && check21 && check22 && check23 && check24)
+    {
+    }else{
+      alert("you must select ratings for both company and category");
+      return false;
+    }
+  }
